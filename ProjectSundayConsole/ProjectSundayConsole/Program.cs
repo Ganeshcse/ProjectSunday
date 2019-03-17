e25fc126db778d2d838169bb42f98830de1cbd63
Ganesh Kumar Poosarla <ganeshcse@live.com>
2019-03-17 23:12:44 +0530

    Pushing PC changes - Removed all stuff just CW & CR

--- ProjectSundayConsole/ProjectSundayConsole/Program.cs
+++ ProjectSundayConsole/ProjectSundayConsole/Program.cs
@@ -11,17 +11,6 @@ namespace ProjectSundayConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo, I have been modified after first checkin!");
-
-            int x = 10 / 2;
-
-            Console.WriteLine("Hallo, I have been modified after first checkin!" + x);
-
-            Console.ReadKey();
-            Console.WriteLine("Hallo, I have been modified after first checkin!");
-            Console.WriteLine("Hallo, I have been modified after first checkin!");
-
-            Console.WriteLine("Hallo, I have been modified after first checkin!");
-
             Console.Read();
         }
     }
